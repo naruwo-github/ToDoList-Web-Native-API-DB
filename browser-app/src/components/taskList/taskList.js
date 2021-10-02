ff62f0149ee1cdd72e6623a0d871f78d859e166f
野川成己 <naruo0711@ab.auone-net.jp>
2021-10-02 21:16:25 +0900

    表による表示

--- browser-app/src/components/taskList/taskList.js
+++ browser-app/src/components/taskList/taskList.js
@@ -1,14 +1,28 @@
 import './taskList.css'
 import React from 'react'
 
-export default function TaskList (props) {
+export default function TaskList ({ tasks, taskLabelTapped }) {
   return (
-    <ul>
-      {props.tasks.map(task => (
-        <li key={task._id}>
-          <label className="clickableLabel" onClick={() => props.taskLabelTapped(task)}>{task.name}</label>
-        </li>
-      ))}
-    </ul>
+    <table>
+      <thead>
+      <tr>
+        <th>_id</th>
+        <th>name</th>
+      </tr>
+      </thead>
+      <tbody>
+      {tasks.map(task =>
+        <tr key={task._id}>
+          <td>{task._id}</td>
+          <td
+            className="clickableLabel"
+            onClick={() => taskLabelTapped(task)}
+          >
+            {task.name}
+          </td>
+        </tr>
+      )}
+      </tbody>
+    </table>
   )
 }
