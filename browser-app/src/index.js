6362f2355d31c569ba6f33c1c4edbf1759a1ba80
野川成己 <naruo0711@ab.auone-net.jp>
2021-10-04 19:13:17 +0900

    routingの実現と/adminによるadmin画面表示

--- browser-app/src/index.js
+++ browser-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import { App } from './components/app'
+import Router from './router/router'
 
 ReactDOM.render(
-  <App />,
+  <Router/>,
   document.getElementById('root')
 )
