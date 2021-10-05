d4223fd09d1c8c64deeb47d79c05cdacbbcedccb
野川成己 <naruo0711@ab.auone-net.jp>
2021-10-05 16:17:36 +0900

    webpackの導入とローカル環境でサーバー起動確認（仮想環境上はこれから）

---
+++ api/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  entry: {
+    server: './server.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js'
+  },
+  target: 'node',
+  node: {
+    // expressを用いて起動するなら必要
+    __dirname: false,
+    __filename: false,
+  },
+  externals: [nodeExternals()], // expressを用いて起動する場合にエラーを回避するため必要
+  module: {
+    rules: [
+      {
+        // ES6~8 to ES5
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
